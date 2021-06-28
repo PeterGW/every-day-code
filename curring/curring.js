7241ac1e091748b1ec73acdc099b0c274d28db3e
PeterGw <wgao1241@163.com>
2021-06-28 23:37:15 +0800

    fix: add annotation

--- curring/curring.js
+++ curring/curring.js
@@ -7,11 +7,15 @@
 function sum(a,b,c,d) {
   return a + b + c + d
 }
-
+// fn.length 是当前函数的参数个数 function.length
 function curring(fn) {
+  // 创建一个空数据 用来保存参数
   let args = []
+  // 给一个内部函数
   const innerFn = (arr = []) => {
+    // 参数保存到数组里面
     args.push(...arr)
+    // 如果当前传入的参数比函数形参多，则返回原函数，否则返回新函数
     return args.length >= fn.length ? fn(...args) : (...args) => innerFn(args)
   }
   return innerFn()
