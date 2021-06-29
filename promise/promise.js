70b619f57309c6ed7c1fb9934cc5b96406acb112
PeterGw <wgao1241@163.com>
2021-06-30 00:21:06 +0800

    add: info

--- promise/promise.js
+++ promise/promise.js
@@ -38,6 +38,10 @@ class Promise {
   }
   
   // 链式调用
+  // 1、then方法中，成功的回调或者失败的回调返回的是一个promise，那么会采用返回的promise的状态，走外层下一次then中的成功或者失败，同时将promise处理结果向下传递
+  // 2、then方法中 成功的回调或者失败的回调返回的是一个普通值（不是promise）讲会返回的结果传递到下一次then的成功中去
+  // 3、如果在then方法的成功或者失败的回调 执行时出错会走到下一次then中的失败中去
+  // 如果返回的是一个失败的promise或者报错，才会走下一个then的是失败，否则全部走成功
   then(onFulfilled, onRejected) {
     if (this.status == FULFILLED) {
       onFulfilled(this.value)
